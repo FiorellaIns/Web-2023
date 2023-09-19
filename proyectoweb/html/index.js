91f9aad03d0dcd6e185ef1584f1bacc5366bc83d
Toto5677 <tomasferreyra40@gmail.com>
2023-09-19 00:09:52 -0300

    19 - 09 - 2023

---
+++ proyectoweb/html/index.js
@@ -0,0 +1,19 @@
+function Encabezado(html)
+{
+    var retorno = html + "<tr>\n";
+    const COLUMNAS = ["Nombre","Apellido","DNI","Nro de afiliado","Obra social","Nro de obra social","Nro de celular","Nro de telefono","Domicilio","Fecha de primera consulta","Fecha de ultima consulta"];
+    for(let i = 0;i<11;i++)
+        retorno = retorno +  "<th>" + COLUMNAS[i] + "</th>" + "\n";
+    return retorno + "</tr>\n";
+}
+
+function agregarArray(array=[],html="")
+{
+    var retorno = html + "<tr>\n";
+    for(let i = 0,cant = array.length;i<cant;i++)
+        retorno = retorno + "<td>" + array[i] + "</td>\n";
+    return retorno + "</tr>\n";
+}
+var tabla = ""
+var tablaDeHTML = document.getElementById("miTabla");
+tablaDeHTML.innerHTML = tabla;
