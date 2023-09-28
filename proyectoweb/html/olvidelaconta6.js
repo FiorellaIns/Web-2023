f6f4841b218618477411e65d318ce94aebe46a8f
valentin127 <143296181+valentin127@users.noreply.github.com>
2023-09-28 09:15:38 -0300

    d
    a

--- proyectoweb/html/olvidelaconta6.js
+++ proyectoweb/html/olvidelaconta6.js
@@ -1,9 +1,23 @@
-var Box1 = document.getElementById("hj");
 var Box1 = document.getElementById("hjñ");
+var Box2 = document.getElementById("hj");
 
 var boton = document.getElementById("boton");
+
+
 
 boton.addEventListener("click",function()
 {
     console.log(Box1.value);
 })
+
+function validateForm() {
+    res = true
+    var x = document.forms["myForm"]["fname"].value;
+    
+    if (x == null || x == "") {
+        alert("Debe completar el campo 'Nombre'");
+        res = false;
+    }
+
+    return res 
+}
