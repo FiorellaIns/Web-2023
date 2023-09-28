97549b9605a60647cc23112a7cad9dfd01e6e6a4
valentin127 <143296181+valentin127@users.noreply.github.com>
2023-09-28 09:13:40 -0300

    a

---
+++ proyectoweb/html/olvidelaconta6.js
@@ -0,0 +1,9 @@
+var Box1 = document.getElementById("hj");
+var Box1 = document.getElementById("hjñ");
+
+var boton = document.getElementById("boton");
+
+boton.addEventListener("click",function()
+{
+    console.log(Box1.value);
+})
