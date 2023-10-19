b594e20e8185b22fc341b875a2fbdaf4d63295e6
Toto5677 <ferreyratomas326@gmail.com>
2023-10-19 10:04:03 -0300

    19-10-2023

--- proyectoweb/html/Js/Añadirpaciente7.js
+++ proyectoweb/html/Js/Añadirpaciente7.js
@@ -1,50 +1,48 @@
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("formulario");
-  console.log(form)
   form.addEventListener("submit", function (event) {
     const nombre = document.getElementById('nombre');
     const apellido = document.getElementById('apellido');
     const nrodeafiliado = document.getElementById('nrodeafiliado');
     const nrodecelular = document.getElementById('nrodecelular');
-    const nrodetelefono = document.getElementById('nrodetelefono');
-    const Domicilio = document.getElementById('Domicilio');
+    const nrodetelefono = document.getElementById('Domicilio');
+    const Domicilio = document.getElementById('Fechadeprimeraconsulta');
 
-    console.log(Domicilio);
+    var retorno = true;
 
-    if (nombre.trim() === "") {
+    if (nombre.value === "") {
       alert("Por favor, ingrese el nombre.");
       event.preventDefault();
       retorno = false;
     }
-    if (apellido.trim() === "") {
+    if (apellido.value === "") {
       alert("Por favor, ingrese el apellido.");
       event.preventDefault();
       retorno = false;
     }
-    if (nrodeafiliado.trim() === "") {
+    if (nrodeafiliado.value === "") {
       alert("Por favor, ingrese el Nro de Afiliado.");
       event.preventDefault();
       retorno = false;
     }
 
-    if (nrodecelular.trim() === "") {
+    if (nrodecelular.value === "") {
       alert("Por favor, ingrese el Nro de Celular.");
       event.preventDefault();
       retorno = false;
     }
 
-    if (nrodetelefono.trim() === "") {
+    if (nrodetelefono.value === "") {
       alert("Por favor, ingrese el Nro de Teléfono.");
       event.preventDefault();
       retorno = false;
     }
-    if (Domicilio.trim() === "") {
+    if (Domicilio.value === "") {
       alert("Por favor, ingrese el Domicilio.");
       event.preventDefault();
       retorno = false;
     }
-    console.log("Hola");
-    event.preventDefault();
+    
     return retorno;
   });
 });
