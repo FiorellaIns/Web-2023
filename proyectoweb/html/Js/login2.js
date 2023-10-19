dd152f3c0d8bb1faaf3e947a6c577db028fe20bf
ftomadoni03 <143636574+ftomadoni03@users.noreply.github.com>
2023-10-19 10:00:38 -0300

    a

--- proyectoweb/html/Js/login2.js
+++ proyectoweb/html/Js/login2.js
@@ -1,27 +1,2 @@
-  document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
 
-    form.addEventListener("submit", function (event) {
-      const contrasena = document.querySelector('#contrasena').value;
-      const usuario = document.querySelector('#usuario').value;
-      var retorno = true;
-      if(usuario.trim() === "" && contrasena.trim() === "")
-      {
-        alert("Por favor, ingrese su usuario y contraseña.");
-        event.preventDefault();
-        retorno = false;
-      }
-      else if (usuario.trim() === "") {
-        alert("Por favor, ingrese su Usuario.");
-        event.preventDefault();
-        retorno = false;
-      }
-      else if (contrasena.trim() === "") {
-        alert("Por favor, ingrese su Contraseña.");
-        event.preventDefault();
-        retorno = false;
-      }
-      return retorno;
-    });
-  });
 
