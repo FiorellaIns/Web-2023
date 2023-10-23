6d378a5e26d3a5d4b0e264444022652f35587b73
valentin127 <143296181+valentin127@users.noreply.github.com>
2023-10-23 20:19:47 -0300

    lastest

--- proyectoweb/html/Js/tablaPrueba.js
+++ proyectoweb/html/Js/tablaPrueba.js
@@ -1,32 +1,36 @@
-const input = document.getElementById("buscador");
-const criterio = document.getElementById("criterio");
-const table = document.getElementById("miTabla");
-
-input.addEventListener("input", function() {
-  const filtro = input.value.toLowerCase();
-  const filas = table.getElementsByTagName("tr");
-
-  for (let i = 1; i < filas.length; i++) {
-    const valorCriterio = filas[i].querySelector(`td:nth-child(${criterio.value})`).textContent.toLowerCase();
-    if (valorCriterio.includes(filtro)) {
-      filas[i].style.display = "";
-    } else {
-      filas[i].style.display = "none";
-    }
-  }
-});
-
 document.addEventListener("DOMContentLoaded", function() {
+  const input = document.getElementById("buscador");
+  const criterio = document.getElementById("criterio");
   const table = document.getElementById("miTabla");
-
-  if (table) {
-    table.addEventListener("click", function(event) {
-      const target = event.target.closest("tr[data-href]");
-      if (target) {
-        const url = target.getAttribute("data-href");
-        window.location.href = url;
+  
+  input.addEventListener("input", function() {
+    const filtro = input.value.toLowerCase();
+    const filas = table.querySelectorAll("tbody tr");
+  
+    filas.forEach(function(fila) {
+      const tds = fila.querySelectorAll("td");
+      let filaCoincide = false;
+      
+      tds.forEach(function(td) {
+        const valorCriterio = td.textContent.toLowerCase();
+        if (valorCriterio.includes(filtro)) {
+          filaCoincide = true;
+        }
+      });
+      
+      if (filaCoincide) {
+        fila.style.display = "";
+      } else {
+        fila.style.display = "none";
       }
     });
-  }
-});
+  });
 
+  table.addEventListener("click", function(event) {
+    const target = event.target.closest("tr[data-href]");
+    if (target) {
+      const url = target.getAttribute("data-href");
+      window.location.href = url;
+    }
+  });
+});
