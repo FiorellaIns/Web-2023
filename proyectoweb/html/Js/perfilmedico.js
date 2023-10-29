9b27107466967dfd0f8d69d7074f6b543f80429d
valentin127 <143296181+valentin127@users.noreply.github.com>
2023-10-29 19:32:14 -0300

    aaa

--- proyectoweb/html/Js/perfilmedico.js
+++ proyectoweb/html/Js/perfilmedico.js
@@ -2,10 +2,21 @@ document.addEventListener("DOMContentLoaded", function() {
     const editarPerfilLink = document.getElementById("siguiente");
   
     if (editarPerfilLink) {
-        editarPerfilLink.style.cursor = "pointer"; // Cambia el cursor al estilo de enlace
+        editarPerfilLink.style.cursor = "pointer";
         editarPerfilLink.addEventListener("click", function() {
             // Redirige a la página de edición
             window.location.href = "editarperfilMedico.html";
         });
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const volver = document.getElementById("volver");
+  
+    if (volver) {
+        volver.style.cursor = "pointer";
+        volver.addEventListener("click", function() {
+            window.location.href = "login.html";
+      });
+    }
+  });
