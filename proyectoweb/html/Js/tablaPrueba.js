f832fe707e752ac86edcbf3bafa129a17e442aae
valentin127 <143296181+valentin127@users.noreply.github.com>
2023-10-30 10:50:08 -0300

    a

--- proyectoweb/html/Js/tablaPrueba.js
+++ proyectoweb/html/Js/tablaPrueba.js
@@ -2,28 +2,29 @@ document.addEventListener("DOMContentLoaded", function() {
   const input = document.getElementById("buscador");
   const criterio = document.getElementById("criterio");
   const table = document.getElementById("miTabla");
-  
+
   input.addEventListener("input", function() {
     const filtro = input.value.toLowerCase();
     const filas = table.querySelectorAll("tbody tr");
-  
-    filas.forEach(function(fila) {
-      const tds = fila.querySelectorAll("td");
+
+    for (let i = 0; i < filas.length; i++) {
+      const tds = filas[i].querySelectorAll("td");
       let filaCoincide = false;
-      
-      tds.forEach(function(td) {
-        const valorCriterio = td.textContent.toLowerCase();
+
+      for (let j = 0; j < tds.length; j++) {
+        const valorCriterio = tds[j].textContent.toLowerCase();
         if (valorCriterio.includes(filtro)) {
           filaCoincide = true;
+          break;
         }
-      });
-      
-      if (filaCoincide) {
-        fila.style.display = "";
-      } else {
-        fila.style.display = "none";
       }
-    });
+
+      if (filaCoincide) {
+        filas[i].style.display = "";
+      } else {
+        filas[i].style.display = "none";
+      }
+    }
   });
 
   table.addEventListener("click", function(event) {
