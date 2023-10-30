2016eb0d50acc2215c290f140b3f3da1eb2ba0e6
valentin127 <143296181+valentin127@users.noreply.github.com>
2023-10-30 12:35:41 -0300

    aaa7

--- proyectoweb/html/Js/registrarse.js
+++ proyectoweb/html/Js/registrarse.js
@@ -1,101 +1,17 @@
-function SwitchDeErroes(ids=0)
-{
-  var retorno = "";
-  switch(ids)
-  {
-    case 0:
-      retorno = "Nombre";
-      break;
-    case 1:
-      retorno = "Apellido";
-      break;
-    case 2:
-      retorno = "DNI valido";
-      break;
-    case 3:
-      retorno = "Matricula valida";
-      break;
-    case 4:
-      retorno = "ambos campos de contraseña";
-      break;
-    default:
-      retorno = undefined;
+function validarFormulario(event) {
+  var nombre = document.getElementById('nombre').value;
+  var apellido = document.getElementById('apellido').value;
+  var dni = document.getElementById('dni').value;
+  var matricula = document.getElementById('matricula').value;
+  var usuario = document.getElementById('usuario').value;
+  var password = document.getElementById('password').value;
+  var confirmPassword = document.getElementById('confirm-password').value;
+
+  if (nombre === "" || apellido === "" || dni === "" || matricula === "" || usuario === "" || password === "" || confirmPassword === "") {
+      document.getElementById('error-message').textContent = "Por favor, complete todos los campos.";
+      event.preventDefault(); // Evitar el envío del formulario si no se completaron todos los campos
+  } else {
+    window.location.href = "login.html";
+      // Si todos los campos están completos, el formulario se enviará automáticamente a "login.html"
   }
-  return retorno;
 }
-
-function ControlDeErrores(cant=0,ids=[])
-{
-  var retorno = "Por favor ingrese ";
-  for(let i = 0;i<cant;i++)
-  {
-    if(i == 0)
-      retorno = retorno + SwitchDeErroes(ids[i]) ;
-    else if(i + 1 < cant)
-      retorno = retorno + ", " + SwitchDeErroes(ids[i]);
-    else if (i + 1 == cant)
-      retorno = retorno + " y " +SwitchDeErroes(ids[i]) + ".";
-  }
-  return retorno;
-}
-
-
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.querySelector("form");
-
-    form.addEventListener("submit", function (event) {
-      var retorno = true;
-      let cantErrores = 0,idErr = [];
-      const NOMBRE = document.querySelector('input[name="Nombre"]').value;
-      const APELLIDO = document.querySelector('input[name="Apellido"]').value;
-      const DNI = document.querySelector('input[name="DNI"]').value;
-      const MATRICULA = document.querySelector('input[name="Numero_de_Afiliado"]').value;
-      const PASSWORD = document.querySelector('input[name="password"]').value;
-      const CONFIRM_PASSWORD = document.querySelector('input[name="confirm-password"]').value;
-      if (NOMBRE.trim() === "") {
-        idErr.push(0);
-        cantErrores++;
-        retorno = false;
-      }
-      if (APELLIDO.trim() === "") {
-        idErr.push(1);
-        cantErrores++;
-        retorno = false;
-      }
-
-      if (DNI.trim() === "" || !/^[\d.]+$/.test(DNI)) {
-        idErr.push(2);
-        cantErrores++;
-        retorno = false;
-      }
-      //buscar error
-      if (MATRICULA.trim() === "" || !/^[\d.]+$/.test(MATRICULA)) {
-        idErr.push(3);
-        cantErrores++;
-        retorno = false;
-    }
-
-      // Validación de las contraseñas
-      if (PASSWORD.trim() === "" || CONFIRM_PASSWORD.trim() === "") {
-        idErr.push(4);
-        cantErrores++;
-        retorno = false;
-      } else if (retorno && PASSWORD !== CONFIRM_PASSWORD) {
-        alert("Las contraseñas no coinciden. Por favor, inténtelo de nuevo.");
-        event.preventDefault();
-        retorno = false;
-      }
-      if(!retorno && cantErrores > 1 )
-      {
-        console.log(cantErrores);
-        alert(ControlDeErrores(cantErrores,idErr));
-        event.preventDefault();
-      }
-      else if(!retorno && cantErrores == 1)
-      {
-        alert("Por favor ingrese " + SwitchDeErroes(idErr[0]) + ".");
-        event.preventDefault();
-      }
-      return retorno;
-    });
-});
