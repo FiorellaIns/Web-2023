f11e38389df0f0c2cca3c3a389f2c49b6d76d5c3
valentin127 <143296181+valentin127@users.noreply.github.com>
2023-11-09 08:25:14 -0300

    a

--- proyectoweb/html/route.py
+++ proyectoweb/html/route.py
@@ -1,34 +1,44 @@
-from flask import Flask,redirect,url_for,request,render_template,jsonify,session
+from flask import Flask, redirect, url_for, request, render_template, jsonify, session
 from SQL_Manejo import IniciarSeccion
 
 error = ""
 
-def Route(aplicacion = Flask):
-    
+def Route(aplicacion=Flask):
+
     @aplicacion.route("/")
     def home():
-        return render_template("login.html",errorVar = error)
-    @aplicacion.route("/GuardarDatosPerfil",methods = ["post"])
+        return render_template("login.html", errorVar=error)
+
+    @aplicacion.route("/GuardarDatosPerfil", methods=["POST"])
     def DeterminarValidez():
         id = -1
         usuario = request.form["email"]
         contrasena = request.form["contrasena"]
-        id = IniciarSeccion([usuario,contrasena])
+        id = IniciarSeccion([usuario, contrasena])
         session["id"] = id
         session["contraseña"] = contrasena
         if id != -1:
             return redirect("/index")
         else:
-            return render_template("login.html",errorVar = "Usuario o contraseña incorrectos. Por favor, inténtelo de nuevo.")
+            return render_template("login.html", errorVar="Usuario o contraseña incorrectos. Por favor, inténtelo de nuevo")
+
     @aplicacion.route("/index")
     def indice():
-        return render_template("index.html") 
-    @aplicacion.route("/consultarseccion",methods=["get"])
+        return render_template("index.html")
+
+    @aplicacion.route("/consultarseccion", methods=["GET"])
     def seccionActual():
-        usuario = {"id":session["id"]}
+        usuario = {"id": session["id"]}
         return jsonify(usuario)
-    
-    
+
+    @aplicacion.route("/registro")
+    def registro():
+        return render_template("registrarse1.html")
+
+    @aplicacion.route("/olvidelacontraseña")
+    def olvidelacontraseña():
+        return render_template("olvidelaconta.html")
+
 
 
 
--- proyectoweb/html/static/js/login2.js
+++ proyectoweb/html/static/js/login2.js
@@ -1,73 +1,43 @@
-const formulario = document.getElementById('login-form');
 const emailinput = document.getElementById('email');
+const formulario = document.getElementById('login-form');
+const expresionEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
 
-const expresionEmail =  /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/ ;
-
-
-emailinput.addEventListener('keyup', validarEmail);
-emailinput.addEventListener('blur', validarEmail);
-
-
-const validarEmail = () => {
+function validarEmail() {
   if (expresionEmail.test(emailinput.value)) {
     document.querySelector('#grupo__email .log__form__input_error').classList.remove('log__form__input_error_activo');
   } else {
     document.querySelector('#grupo__email .log__form__input_error').classList.add('log__form__input_error_activo');
   }
-};
+}
 
 formulario.addEventListener('submit', (e) => {
   e.preventDefault();
-
   if (expresionEmail.test(emailinput.value)) {
     formulario.reset();
-    window.location.href = 'index.html';
-  } 
+
+    if (window.location.href.indexOf("http://") === 0 || window.location.href.indexOf("https://") === 0) {
+      window.location.href = "/";
+    } else {
+      window.location.href = "index.html";
+    }
+  }
 });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const formulario = document.getElementById("login-form");
-  const mensajeError = document.getElementById("mensaje-error");
+const olvideContraLink = document.getElementById("olvideContra");
+const registrarseLink = document.getElementById("registrarse");
 
-  formulario.addEventListener("submit", function (event) {
-    window.location.href="index.html";
-  });
+olvideContraLink.addEventListener("click", function () {
+  if (window.location.href.indexOf("http://") === 0 || window.location.href.indexOf("https://") === 0) {
+    window.location.href = "/olvidelacontraseña";
+  } else {
+    window.location.href = "olvidelaconta.html";
+  }
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-  const olvideContraLink = document.getElementById("olvideContra");
-  const registrarseLink = document.getElementById("registrarse");
-
-  olvideContraLink.addEventListener("click", function() {
-      window.location.href = "olvidelaconta.html"; 
-  });
-
-  registrarseLink.addEventListener("click", function() {
-      window.location.href = "registrarse1.html"; 
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  const formulario = document.getElementById("login-form");
-  const mensajeError = document.getElementById("mensaje-error");
-
-  formulario.addEventListener("submit", function (event) {
-    const urlRegistro = window.location.origin + "/registro";
-    window.location.href = urlRegistro;
-  });
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-  const olvideContraLink = document.getElementById("olvideContra");
-  const registrarseLink = document.getElementById("registrarse");
-
-  olvideContraLink.addEventListener("click", function() {
-    const urlOlvidelaContra = window.location.origin + "/olvidelacontraseña";
-    window.location.href = urlOlvidelaContra;
-  });
-
-  registrarseLink.addEventListener("click", function() {
-    const urlRegistro = window.location.origin + "/registro";
-    window.location.href = urlRegistro;
-  });
+registrarseLink.addEventListener("click", function () {
+  if (window.location.href.indexOf("http://") === 0 || window.location.href.indexOf("https://") === 0) {
+    window.location.href = "/registro";
+  } else {
+    window.location.href = "registrarse1.html";
+  }
 });
