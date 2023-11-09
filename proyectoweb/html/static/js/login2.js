0487bb1ba5ace12048ace87fd8b30bd9a0a69742
valentin127 <143296181+valentin127@users.noreply.github.com>
2023-11-09 00:05:08 -0300

    q

--- proyectoweb/html/static/js/login2.js
+++ proyectoweb/html/static/js/login2.js
@@ -1,69 +1,48 @@
-/*const formulario = document.getElementById('login-form');
-const emailinput = document.getElementById('email');
-
-const expresionEmail =  /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/ ;
-
-
-emailinput.addEventListener('keyup', validarEmail);
-emailinput.addEventListener('blur', validarEmail);
-
-
-const validarEmail = () => {
-  if (expresionEmail.test(emailinput.value)) {
-    document.querySelector('#grupo__email .log__form__input_error').classList.remove('log__form__input_error_activo');
-  } else {
-    document.querySelector('#grupo__email .log__form__input_error').classList.add('log__form__input_error_activo');
-  }
-};
-
-formulario.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  if (expresionEmail.test(emailinput.value)) {
-    formulario.reset();
-    window.location.href = 'index.html';
-  } 
-});*/
-
-
-document.addEventListener("DOMContentLoaded", function() {
-  const olvideContraLink = document.getElementById("olvideContra");
-  const registrarseLink = document.getElementById("registrarse");
-
-  olvideContraLink.addEventListener("click", function() {
-    window.location.href = "olvidelaconta.html";
-  });
-
-  registrarseLink.addEventListener("click", function() {
-    window.location.href = "registrarse1.html";
-  });
-});
-
-
-
-/*document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
   const formulario = document.getElementById("login-form");
-  const mensajeError = document.getElementById("mensaje-error");
+  const emailinput = document.getElementById("email");
+  const expresionEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
 
-  formulario.addEventListener("submit", function (event) {
-    const urlRegistro = window.location.origin + "/registro";
-    window.location.href = urlRegistro;
+  emailinput.addEventListener('keyup', validarEmail);
+  emailinput.addEventListener('blur', validarEmail);
+
+  function validarEmail() {
+    if (expresionEmail.test(emailinput.value)) {
+      document.querySelector('#grupo_email .log__form__input_error').classList.remove('log__form__input_error_activo');
+    } else {
+      document.querySelector('#grupo_email .log__form__input_error').classList.add('log__form__input_error_activo');
+    }
+  }
+
+  formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (expresionEmail.test(emailinput.value)) {
+      formulario.reset();
+      if (window.location.href.indexOf("http://") === 0 || window.location.href.indexOf("https://") === 0) {
+        window.location.href = "/";
+      } else {
+        window.location.href = "index.html";
+      }
+    }
   });
-});
 
-document.addEventListener("DOMContentLoaded", function() {
   const olvideContraLink = document.getElementById("olvideContra");
   const registrarseLink = document.getElementById("registrarse");
 
   olvideContraLink.addEventListener("click", function() {
-    const urlOlvidelaContra = window.location.origin + "/olvidelacontraseña";
-    window.location.href = urlOlvidelaContra;
+    if (window.location.href.indexOf("http://") === 0 || window.location.href.indexOf("https://") === 0) {
+      window.location.href = "/olvidelacontraseña";
+    } else {
+      window.location.href = "olvidelaconta.html";
+    }
   });
 
   registrarseLink.addEventListener("click", function() {
-    const urlRegistro = window.location.origin + "/registro";
-    window.location.href = urlRegistro;
+    if (window.location.href.indexOf("http://") === 0 || window.location.href.indexOf("https://") === 0) {
+      window.location.href = "/registro";
+    } else {
+      window.location.href = "registrarse1.html";
+    }
   });
-});*/
-
-
+});
