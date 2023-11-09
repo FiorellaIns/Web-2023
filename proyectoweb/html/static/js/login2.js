cb40bfb2c004ca32f8f4eae7ecd7d94c2ef78a70
valentin127 <143296181+valentin127@users.noreply.github.com>
2023-11-08 23:29:21 -0300

    aa

--- proyectoweb/html/static/js/login2.js
+++ proyectoweb/html/static/js/login2.js
@@ -19,3 +19,30 @@ document.addEventListener("DOMContentLoaded", function() {
       window.location.href = "registrarse1.html"; 
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const formulario = document.getElementById("login-form");
+  const mensajeError = document.getElementById("mensaje-error");
+
+  formulario.addEventListener("submit", function (event) {
+    const urlRegistro = window.location.origin + "/registro";
+    window.location.href = urlRegistro;
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  const olvideContraLink = document.getElementById("olvideContra");
+  const registrarseLink = document.getElementById("registrarse");
+
+  olvideContraLink.addEventListener("click", function() {
+    const urlOlvidelaContra = window.location.origin + "/olvidelacontraseña";
+    window.location.href = urlOlvidelaContra;
+  });
+
+  registrarseLink.addEventListener("click", function() {
+    const urlRegistro = window.location.origin + "/registro";
+    window.location.href = urlRegistro;
+  });
+});
+
+
