431ddd7661e9f4c02d37adde0214ea7d246383f2
FiorellaIns <88846881+FiorellaIns@users.noreply.github.com>
2023-11-09 00:14:46 -0300

    a

--- proyectoweb/html/static/js/login2.js
+++ proyectoweb/html/static/js/login2.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> parent of 588cdcd (aa)
 const formulario = document.getElementById('login-form');
 const emailinput = document.getElementById('email');
 
@@ -28,66 +23,6 @@ formulario.addEventListener('submit', (e) => {
     formulario.reset();
     window.location.href = 'index.html';
   } 
-<<<<<<< HEAD
-});
-
->>>>>>> parent of 588cdcd (aa)
-document.addEventListener("DOMContentLoaded", function () {
-  const formulario = document.getElementById("login-form");
-  const emailinput = document.getElementById("email");
-  const expresionEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
-
-<<<<<<< HEAD
-  emailinput.addEventListener('keyup', validarEmail);
-  emailinput.addEventListener('blur', validarEmail);
-
-  function validarEmail() {
-    if (expresionEmail.test(emailinput.value)) {
-      document.querySelector('#grupo_email .log__form__input_error').classList.remove('log__form__input_error_activo');
-    } else {
-      document.querySelector('#grupo_email .log__form__input_error').classList.add('log__form__input_error_activo');
-    }
-  }
-
-  formulario.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    if (expresionEmail.test(emailinput.value)) {
-      formulario.reset();
-      if (window.location.href.indexOf("http://") === 0 || window.location.href.indexOf("https://") === 0) {
-        window.location.href = "/";
-      } else {
-        window.location.href = "index.html";
-      }
-    }
-=======
-  formulario.addEventListener("submit", function (event) {
-    window.location.href="index.html";
->>>>>>> parent of 588cdcd (aa)
-  });
-
-  const olvideContraLink = document.getElementById("olvideContra");
-  const registrarseLink = document.getElementById("registrarse");
-
-  olvideContraLink.addEventListener("click", function() {
-<<<<<<< HEAD
-    if (window.location.href.indexOf("http://") === 0 || window.location.href.indexOf("https://") === 0) {
-      window.location.href = "/olvidelacontraseña";
-    } else {
-      window.location.href = "olvidelaconta.html";
-    }
-  });
-
-  registrarseLink.addEventListener("click", function() {
-    if (window.location.href.indexOf("http://") === 0 || window.location.href.indexOf("https://") === 0) {
-      window.location.href = "/registro";
-    } else {
-      window.location.href = "registrarse1.html";
-    }
-  });
-});
-=======
-=======
 });
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -102,18 +37,37 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function() {
   const olvideContraLink = document.getElementById("olvideContra");
   const registrarseLink = document.getElementById("registrarse");
-  
+
   olvideContraLink.addEventListener("click", function() {
->>>>>>> parent of 588cdcd (aa)
-    window.location.href = "olvidelaconta.html"; 
+      window.location.href = "olvidelaconta.html"; 
   });
 
   registrarseLink.addEventListener("click", function() {
-    window.location.href = "registrarse1.html"; 
+      window.location.href = "registrarse1.html"; 
   });
-<<<<<<< HEAD
 });
->>>>>>> parent of 588cdcd (aa)
-=======
+
+document.addEventListener("DOMContentLoaded", function () {
+  const formulario = document.getElementById("login-form");
+  const mensajeError = document.getElementById("mensaje-error");
+
+  formulario.addEventListener("submit", function (event) {
+    const urlRegistro = window.location.origin + "/registro";
+    window.location.href = urlRegistro;
+  });
 });
->>>>>>> parent of 588cdcd (aa)
+
+document.addEventListener("DOMContentLoaded", function() {
+  const olvideContraLink = document.getElementById("olvideContra");
+  const registrarseLink = document.getElementById("registrarse");
+
+  olvideContraLink.addEventListener("click", function() {
+    const urlOlvidelaContra = window.location.origin + "/olvidelacontraseña";
+    window.location.href = urlOlvidelaContra;
+  });
+
+  registrarseLink.addEventListener("click", function() {
+    const urlRegistro = window.location.origin + "/registro";
+    window.location.href = urlRegistro;
+  });
+});
