3c5edb04012dbc0007d21fe7f9fbc5785fc283eb
ftomadoni03 <143636574+ftomadoni03@users.noreply.github.com>
2023-11-08 22:48:20 -0300

    aaaaa

--- proyectoweb/html/static/js/login2.js
+++ proyectoweb/html/static/js/login2.js
@@ -1,3 +1,30 @@
+const formulario = document.getElementById('login-form');
+const emailinput = document.getElementById('email');
+
+const expresionEmail =  /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/ ;
+
+
+emailinput.addEventListener('keyup', validarEmail);
+emailinput.addEventListener('blur', validarEmail);
+
+
+const validarEmail = () => {
+  if (expresionEmail.test(emailinput.value)) {
+    document.querySelector('#grupo__email .log__form__input_error').classList.remove('log__form__input_error_activo');
+  } else {
+    document.querySelector('#grupo__email .log__form__input_error').classList.add('log__form__input_error_activo');
+  }
+};
+
+formulario.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (expresionEmail.test(emailinput.value)) {
+    formulario.reset();
+    window.location.href = 'index.html';
+  } 
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   const formulario = document.getElementById("login-form");
   const mensajeError = document.getElementById("mensaje-error");
