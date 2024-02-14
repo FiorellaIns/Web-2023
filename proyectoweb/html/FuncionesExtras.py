9a923ab38980af759a0c95e309b24fffd7f231df
Toto5677 <ferreyratomas326@gmail.com>
2024-02-14 11:41:18 -0300

    14 / 02 / 2024

--- proyectoweb/html/FuncionesExtras.py
+++ proyectoweb/html/FuncionesExtras.py
@@ -10,5 +10,5 @@ def ConvertirADiccionario(lista=[]):
     retorno = {"exito":False}
     if len(lista) == 10:
         retorno = {"exito":True,"ID":lista[0],"Nombre":lista[1],"Apellido":lista[2],"DNI":lista[3],"Nro de afiliado":lista[4]
-                   ,"Obra social":lista[5],"Nro de obra social":lista[6],"Nro de telefono":lista[7],"Domicilo":lista[8],"Fecha de consulta":lista[9]}
+                   ,"Obra social":lista[5],"Nro de obra social":lista[6],"Nro de telefono":lista[7],"Domicilo":lista[8],"Fecha de consulta":lista[9].strftime('%d/%m/%Y')}
     return retorno
