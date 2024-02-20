f86d1d273820d20d04df4bd1aab3998d10eb22e8
valentin127 <143296181+valentin127@users.noreply.github.com>
2024-02-20 18:59:28 -0300

    cambios en la TablaAdmin
    solo son cambios practicos, necesito crearle el boton eliminar funcional

--- proyectoweb/html/static/js/tablaAdm.js
+++ proyectoweb/html/static/js/tablaAdm.js
@@ -1,28 +1,15 @@
 const SOLICITUDHECHA = 4;
 const RESPUESTAEXITOSA = 200;
 
-var datosRecibidos;
-
 document.addEventListener("DOMContentLoaded", function() {
   const input = document.getElementById("buscador");
   const criterio = document.getElementById("criterio");
   const table = document.getElementById("miTabla");
 
   input.addEventListener("input", function() {
-    nuevo = BuscarEnLista(input.value, datosRecibidos, criterio.value);
-    ActualizarTabla(nuevo);
+    const nuevo = BuscarEnLista(input.value, datosRecibidos, criterio.value);
+    ActualizarTabla(nuevo, datosRecibidos, usuariosSeleccionados);
   });
-
-  const peticion = new XMLHttpRequest();
-  peticion.open("GET", "/Datos_usuarios");
-  peticion.onreadystatechange = function() {
-    if (peticion.readyState === SOLICITUDHECHA && peticion.status === RESPUESTAEXITOSA) {
-      const respuesta = JSON.parse(peticion.responseText);
-      ActualizarTabla(respuesta);
-      datosRecibidos = respuesta;
-    }
-  };
-  peticion.send();
 
   const volver = document.getElementById("volver");
   volver.addEventListener("click", function(event) {
@@ -30,43 +17,87 @@ document.addEventListener("DOMContentLoaded", function() {
     window.location.href = url;
   });
 
+  const usuariosSeleccionados = [];
   table.addEventListener("change", function(event) {
     if (event.target.type === "checkbox") {
       const checkboxId = event.target.id;
-      const userId = checkboxId.split("_")[1]; // Obtener el ID de usuario de la casilla de verificación
+      const userId = checkboxId.split("_")[1];
       const usuarioSeleccionado = datosRecibidos.find(usuario => usuario.ID === parseInt(userId));
-      
-      
-      // Desmarcar todas las casillas de verificación excepto la seleccionada
-      const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-      checkboxes.forEach(function(checkbox) {
-        if (checkbox.id !== checkboxId) {
-          checkbox.checked = false;
+
+      if (event.target.checked) {
+        usuariosSeleccionados.push(usuarioSeleccionado);
+      } else {
+        const index = usuariosSeleccionados.findIndex(u => u.ID === usuarioSeleccionado.ID);
+        if (index !== -1) {
+          usuariosSeleccionados.splice(index, 1);
         }
-      });
-      
-      console.log("Medico seleccionado:", usuarioSeleccionado.Nombre,usuarioSeleccionado.Apellido,",con usuario:",usuarioSeleccionado.Usuario,"y ID:",userId);
-      // futuras acciones adicionales que desees con el usuario seleccionado
+      }
+
+      if (usuariosSeleccionados.length > 0) {
+        console.log("Usuarios seleccionados:", usuariosSeleccionados);
+      }
     }
   });
+
+  const boton = document.getElementById("eliminarusuario");
+  boton.addEventListener("click", function() {
+    eliminar(usuariosSeleccionados);
+  });
+
+  const peticion = new XMLHttpRequest();
+  peticion.open("GET", "/Datos_usuarios");
+  peticion.onreadystatechange = function() {
+    if (peticion.readyState === SOLICITUDHECHA && peticion.status === RESPUESTAEXITOSA) {
+      const respuesta = JSON.parse(peticion.responseText);
+      ActualizarTabla(respuesta, respuesta, usuariosSeleccionados);
+    }
+  };
+  peticion.send();
 });
 
-function ActualizarTabla(diccionarios = []) {
-  let lectura = ""
+function ActualizarTabla(diccionarios = [], datosRecibidos, usuariosSeleccionados) {
+  let lectura = "";
   const cuerpoTabla = document.getElementById("cuerpo");
-  for (let i = 0; i < diccionarios.length; i++)
-    lectura = lectura + ConstruirStringTabla(diccionarios[i]);
+  cuerpoTabla.innerHTML = "";
+
+  for (let i = 0; i < diccionarios.length; i++) {
+    lectura += ConstruirStringTabla(diccionarios[i]);
+  }
   cuerpoTabla.innerHTML = lectura;
+
+  const filas = document.querySelectorAll(".fila");
+  filas.forEach(function(fila) {
+    fila.addEventListener("click", function(event) {
+      const checkbox = fila.querySelector("input[type='checkbox']");
+      checkbox.checked = !checkbox.checked;
+      const userId = checkbox.id.split("_")[1];
+      const usuarioSeleccionado = datosRecibidos.find(usuario => usuario.ID === parseInt(userId));
+
+      if (checkbox.checked) {
+        usuariosSeleccionados.push(usuarioSeleccionado);
+      } else {
+        const index = usuariosSeleccionados.findIndex(u => u.ID === usuarioSeleccionado.ID);
+        if (index !== -1) {
+          usuariosSeleccionados.splice(index, 1);
+        }
+      }
+
+      if (usuariosSeleccionados.length > 0) {
+        console.log("Usuarios seleccionados:", usuariosSeleccionados);
+      }
+    });
+  });
 }
 
 function ConstruirStringTabla(diccionario = {}) {
   const datos = ["Nombre", "Apellido", "DNI", "Matricula medica", "Usuario", "Contraseña", "EMail"];
-  var retorno = "<tr class=\"fila\">";
-  let id = diccionario["ID"];
+  let retorno = "<tr class=\"fila\">";
+  const id = diccionario["ID"];
   retorno += "<td><input type=\"checkbox\" class=\"checkFila\" id=\"checkbox_" + id + "\"></td>";
-  for (let i = 0; i < datos.length; i++)
-    retorno = retorno + ConstruirStringColumna(id, diccionario[datos[i]]);
-  retorno = retorno + "</tr>";
+  for (let i = 0; i < datos.length; i++) {
+    retorno += ConstruirStringColumna(id, diccionario[datos[i]]);
+  }
+  retorno += "</tr>";
   return retorno;
 }
 
@@ -75,31 +106,23 @@ function ConstruirStringColumna(id, dato) {
 }
 
 function BuscarEnLista(palabra = "", diccionario = [], filtro = "") {
-  var retorno = [];
+  const retorno = [];
   for (let i = 0, lon = diccionario.length; i < lon; i++) {
-    if (VerificarIgualdad(palabra, diccionario[i][filtro]))
+    if (VerificarIgualdad(palabra, diccionario[i][filtro])) {
       retorno.push(diccionario[i]);
+    }
   }
   return retorno;
 }
 
 function VerificarIgualdad(palabra = "", rec) {
-  var sAux = "" + rec;
+  const sAux = "" + rec;
   return sAux.includes(palabra);
 }
 
-
-//programar...
-function editar(){
-    const boton = document.getElementById("editarusuario")
-    boton.addEventListener("click",function(){
-        
-    });
-}
-
-function eliminar(){
-    const boton = document.getElementById("eliminarusuario")
-    boton.addEventListener("click",function(){
-
-    });
+function eliminar(usuariosSeleccionados) {
+  // Aquí deberías enviar una solicitud al servidor para eliminar los usuarios seleccionados
+  // Por ejemplo, podrías usar una solicitud XMLHttpRequest o fetch para enviar una solicitud DELETE al servidor
+  // y pasar los IDs de los usuarios como parámetros en la URL o en el cuerpo de la solicitud
+  // Una vez que se eliminen los usuarios, puedes recargar la página o actualizar la tabla de usuarios
 }
