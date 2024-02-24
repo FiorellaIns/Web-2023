414f694c2ce827cd9b4f39036ae06724c698f186
FiorellaIns <88846881+FiorellaIns@users.noreply.github.com>
2024-02-23 21:35:31 -0300

    a

--- proyectoweb/html/route.py
+++ proyectoweb/html/route.py
@@ -320,4 +320,18 @@ def Route(aplicacion=Flask):
             return redirect(url_for("home"))
 
 
-        
+    @aplicacion.route("/enviarDiagnostico", methods=["POST"])
+    def Enviardatosdiagnostico():
+            respuesta = ""
+            listaDeDatos = [request.form.get("Fecha"),
+                            request.form.get("motivo"),
+                            request.form.get("nombreM"),
+                            request.form.get("DiagnosticoMedico"),
+                            request.form.get("Descripcion")]
+
+            if EstaCompleto(listaDeDatos):
+                Datosdediagnostico(listaDeDatos)
+                respuesta = "Hecho"
+            else:
+                respuesta = "No se recibieron todos los datos."
+            return jsonify({"respuesta": respuesta})
