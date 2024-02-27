7664bbed91c7852c99d0af87e8cbf83709309d3e
FiorellaIns <88846881+FiorellaIns@users.noreply.github.com>
2024-02-26 22:02:09 -0300

    CAMBIOS

--- proyectoweb/html/SQL_Manejo.py
+++ proyectoweb/html/SQL_Manejo.py
@@ -184,7 +184,6 @@ def Datosdediagnostico(lista=[],id_medico = 0,id_paciente = 0):
     conexion = InicializarConexion()
     herramienta = conexion.cursor()
     longitud = len(lista)
-    print(lista)
     if longitud == 5:  
         comando = "INSERT INTO `historias clinicas` (ID,`Diagnostico medico`, Descripcion, `Fecha de atencion`, `Motivo de atencion`,`ID medico`,`ID paciente`) VALUES (%s,%s,%s,%s,%s,%s,%s)"
         argumentos = (None, lista[3], lista[4], datetime.now().date(), lista[1],id_medico,id_paciente)  # Ajuste de argumentos
